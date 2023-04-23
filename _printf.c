108e980d00503cbc907f4295a03d501ea4a23bec
Gabula Pius <mcduell24@gmail.com>
2023-04-23 15:21:40 +0300

    printf boilerplate code

---
+++ _printf.c
@@ -0,0 +1,18 @@
+/*
+ * Authors: pius,
+ *					timothy
+ */
+
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: character format specifier
+ * Return: Always 0 (Success)
+ */
+int _printf(const char *format, ...)
+{
+
+	return (0);
+}
+
